aa490d6743f95d1f729b64d22ce25ac45d16b8d9
Ivan Dobkov <idobkov@gmail.com>
2025-01-06 16:30:37 +0200

    adding OneToMany bidirectional relation

--- src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 @Builder
 @Getter
@@ -28,4 +29,6 @@ public class Customer {
     private int version;
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
+    @OneToMany(mappedBy = "customer")
+    private Set<BeerOrder> beerOrders;
 }
