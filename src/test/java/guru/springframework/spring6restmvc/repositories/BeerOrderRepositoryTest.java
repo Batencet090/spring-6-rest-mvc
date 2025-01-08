49e37b2cd316ad8b43afccf99bbcd6741f352c6a
Ivan Dobkov <idobkov@gmail.com>
2025-01-08 14:19:56 +0200

    persisting BeerOrder relationships

---
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.entities.BeerOrder;
+import guru.springframework.spring6restmvc.entities.Customer;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerOrderRepositoryTest {
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+    @Autowired
+    BeerRepository beerRepository;
+    Customer testCustomer;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testCustomer = customerRepository.findAll().getFirst();
+        testBeer = beerRepository.findAll().getFirst();
+
+    }
+
+    @Transactional
+    @Test
+    void testBeerOrders() {
+        BeerOrder beerOrder = BeerOrder.builder()
+                .customerRef("Test order")
+                .customer(testCustomer)
+                .build();
+
+        BeerOrder savedBeerOrder = beerOrderRepository.saveAndFlush(beerOrder);
+        System.out.println(savedBeerOrder.getCustomerRef());
+    }
+
+}
